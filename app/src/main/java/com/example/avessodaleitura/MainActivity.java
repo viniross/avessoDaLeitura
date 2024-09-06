3ec3a464ede158e31151dccb999b660ad4418a63
Vinícius Ross <viniciusrossws@gmail.com>
2024-09-06 09:20:05 -0300

    Start of animations between 2 activities

--- app/src/main/java/com/example/avessodaleitura/MainActivity.java
+++ app/src/main/java/com/example/avessodaleitura/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.avessodaleitura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView capaLivro = findViewById(R.id.capaLivro);
+
+        capaLivro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaMenu = new Intent(MainActivity.this, Menu.class);
+                startActivity(telaMenu);
+                overridePendingTransition(R.anim.);
+            }
+        });
+
     }
 }
