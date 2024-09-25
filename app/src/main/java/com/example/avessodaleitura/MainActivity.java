1c15aebfa57be2d24a61c24aead12a3f68869e0d
Vinícius Ross <viniciusrossws@gmail.com>
2024-09-25 10:15:22 -0300

    Code of storytelling is bugged

--- app/src/main/java/com/example/avessodaleitura/MainActivity.java
+++ app/src/main/java/com/example/avessodaleitura/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.avessodaleitura;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.widget.ViewPager2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
-import com.example.avessodaleitura.MenuActivity;
-import com.example.avessodaleitura.SinopseActivity;
-import com.example.avessodaleitura.AutorActivity;
-import com.example.avessodaleitura.OpiniaoActivity;
-import com.example.avessodaleitura.;
-import com.example.avessodaleitura.PersonagensActivity;
-import com.example.avessodaleitura.TrechosActivity;
-import com.example.avessodaleitura.TimeLineActivity1;
-import com.example.avessodaleitura.TimeLineActivity2;
-import com.example.avessodaleitura.FofocaActivity;
+import com.example.avessodaleitura.fragments.MenuActivity;
+import com.example.avessodaleitura.fragments.SinopseActivity;
+import com.example.avessodaleitura.fragments.AutorActivity;
+import com.example.avessodaleitura.fragments.OpiniaoActivity;
+import com.example.avessodaleitura.*;
+import com.example.avessodaleitura.fragments.PersonagensActivity;
+import com.example.avessodaleitura.fragments.TrechosActivity;
+import com.example.avessodaleitura.fragments.TimeLineActivity1;
+import com.example.avessodaleitura.fragments.TimeLineActivity2;
+import com.example.avessodaleitura.fragments.FofocaActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView capaLivro = findViewById(R.id.capaLivro);
+        viewPager = findViewById(R.id.viewPager);
+
+        adapter = new ViewPagerAdapter(this);
+        adapter.addFragment(new MenuActivity());
+        adapter.addFragment(new SinopseActivity());
+        adapter.addFragment(new AutorActivity());
+        adapter.addFragment(new OpiniaoActivity());
+        adapter.addFragment(new PersonagensActivity());
+        adapter.addFragment(new TrechosActivity());
+        adapter.addFragment(new TimeLineActivity1());
+        adapter.addFragment(new TimeLineActivity2());
+        adapter.addFragment(new FofocaActivity());
 
         capaLivro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +57,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private static class ViewPagerAdapter extends FragmentStateAdapter {
+
+        private final List<Fragment> fragmentList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        public void addFragment(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragmentList.size();
+        }
     }
 }
---
+++ app/src/main/java/com/example/avessodaleitura/fragments/TrechosActivity.java
@@ -0,0 +1,20 @@
+package com.example.avessodaleitura.fragments;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.avessodaleitura.R;
+
+public class TrechosActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Infla o layout para este fragmento
+        return inflater.inflate(R.layout.activity_trechos, container, false);
+    }
+}
