44bb0b1c0bc6ec30ec2ad6058ae7a3e3261c01af
Artur Teixeira dos Santos <artur2006santos@gmail.com>
2024-09-28 10:07:41 -0300

    sinopse finalizada, e autor faltando apenas título

--- app/src/main/java/com/example/avessodaleitura/SinopseActivity.java
+++ app/src/main/java/com/example/avessodaleitura/SinopseActivity.java
@@ -2,13 +2,26 @@ package com.example.avessodaleitura;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SinopseActivity extends AppCompatActivity {
 
+    ImageView BtnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sinopse);
+
+        BtnHome = findViewById(R.id.BtnHome);
+        BtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent menu = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(menu);
+            }
+        });
     }
 }
