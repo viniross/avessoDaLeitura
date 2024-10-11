176372408cec143a93e55ea838d280d1ea518f27
Anna Lia Lira <annalialira@gmail.com>
2024-10-11 10:12:11 -0300

    Botões

--- app/src/main/java/com/example/avessodaleitura/Opiniao1Activity.java
+++ app/src/main/java/com/example/avessodaleitura/Opiniao1Activity.java
@@ -6,7 +6,6 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Opiniao1Activity extends AppCompatActivity {
